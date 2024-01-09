2b94603fd10b2e2ee2da3c6b54d0faf49e5e00b1
Leo <drd9811@naver.com>
2024-01-09 17:39:24 +0900

    Edit data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO calalog(product_id, product_name, stock, unit_price) VALUES ('CATALOG_001', 'Berlin', 100, 1500);
-INSERT INTO calalog(product_id, product_name, stock, unit_price) VALUES ('CATALOG_002', 'Tokyo', 110, 1000);
-INSERT INTO calalog(product_id, product_name, stock, unit_price) VALUES ('CATALOG_003', 'Stockholm', 120, 2000);
+INSERT INTO catalog(product_id, product_name, stock, unit_price) VALUES ('CATALOG_001', 'Berlin', 100, 1500);
+INSERT INTO catalog(product_id, product_name, stock, unit_price) VALUES ('CATALOG_002', 'Tokyo', 110, 1000);
+INSERT INTO catalog(product_id, product_name, stock, unit_price) VALUES ('CATALOG_003', 'Stockholm', 120, 2000);
